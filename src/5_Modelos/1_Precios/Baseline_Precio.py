fcd94656695ce9c5fd7a7d6e6ec75a39aea94da0
ouwen-yx <ouyang@ucm.es>
2026-03-22 17:25:37 +0100

    Ajustes baseline_precios
    RMSE directamente y np.full

--- src/5_Modelos/1_Precios/Baseline_Precio.py
+++ src/5_Modelos/1_Precios/Baseline_Precio.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import wandb
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from sklearn.metrics import mean_absolute_error, root_mean_squared_error, r2_score
 
 from utils.funciones_minio import crear_cliente_minio, bajar_minio
 
@@ -22,11 +22,11 @@ def calculo_medias(df, nombre_mercado):
     media_entrenamiento = y_train.mean()
 
     # Vector de predicciones del baseline (la media del entrenamiento, para todos los casos)
-    y_pred_baseline = np.full_like(y_test, fill_value=media_entrenamiento)
+    y_pred_baseline = np.full(y_test.shape, fill_value=media_entrenamiento)
 
     # Cálculo de métricas
     mae_base = mean_absolute_error(y_test, y_pred_baseline)
-    rmse_base = np.sqrt(mean_squared_error(y_test, y_pred_baseline))
+    rmse_base = root_mean_squared_error(y_test, y_pred_baseline)
     r2_base = r2_score(y_test, y_pred_baseline)
 
     run = wandb.init(
@@ -54,4 +54,3 @@ if __name__ == "__main__":
 
     calculo_medias(df_venta, "venta")
     calculo_medias(df_alquiler, "alquiler")
-    
