327016b1caa8ab32cfa53f5f570f585239d7be39
arthur-112 <traiacob@ucm.es>
2026-04-07 23:48:44 +0200

    Update modelo_knn.py

--- src/5_Modelos/1_Precios/modelo_knn.py
+++ src/5_Modelos/1_Precios/modelo_knn.py
@@ -234,5 +234,5 @@ if __name__ == "__main__":
     df_venta = bajar_minio(cliente, "dataset_ml/precios/ventas", "df_venta_limpio.parquet")
     df_alquiler = bajar_minio(cliente, "dataset_ml/precios/alquiler", "df_alquiler_limpio.parquet")
 
-    entrenar_knn(df_venta, "venta")
+    #entrenar_knn(df_venta, "venta")
     entrenar_knn(df_alquiler, "alquiler")
